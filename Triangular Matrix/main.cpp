35aea8a6bb2d805bbc6a3a7cb102bdf1a432b675
wsemnazlo <92176898+wsemnazlo@users.noreply.github.com>
2022-10-07 01:44:59 +0300

    Add files via upload

--- Triangular Matrix/main.cpp
+++ Triangular Matrix/main.cpp
@@ -1,53 +1,34 @@
 #include <iostream>
 #include <vector>
 
-#define SIZE 3
+#define SIZE 3 // Matrix size change. Use only square matrix!
 
 using namespace std;
 
 void Menu()
 {
-    cout << "1 - Enter vector." << endl << "2 - Enter matrix A." << endl << "3 - Enter matrix B." << endl << "4 - Transpose matrix;" << endl;
+    cout << endl << "1) Enter Matrix A." << endl;
+    cout << "2) Enter Matrix B." << endl ;
+    cout << "3) Matrix A + Matrix B." << endl;
+    cout << "4) Matrix A - Matrix B."  << endl;
+    cout << "5) Matrix A * Matrix B." << endl;
+    cout << "6) Transpose Matrix A and Matrix B." << endl << endl;
+    cout << "0) Exit." << endl << endl;
 }
 
-template<typename T>
-void printMatrixOne(vector<vector<T>> mat)
-{
-    for (int i = 0; i < mat.size(); i++)
-    {
-        for (int j = 0; j < mat[i].size(); j++)
-            cout << mat[i][j] << " ";
-        cout << endl;
-    }
-}
-
-template<typename T>
-void printMatrixTwo(vector<vector<T>> mat)
-{
-    for (int i = 0; i < mat.size(); i++)
-        cout << mat[i][0] << " ";
-    cout << endl;
-}
-
-template<typename T>
-void iMatrixOne(vector<vector<T>> &mat)
+void iMatrix(vector<vector<int>> &mat)
 {
     for (int i = 0; i < mat.size(); i++)
     {
         for (int j = i; j < mat.size(); j++)
+        {
+            cout << "Enter element Triangular Matrix: ";
             cin >> mat[i][j];
+        }
     }
 }
 
-template<typename T>
-void iMatrixTwo(vector<vector<T>> &mat)
-{
-    for (int i = 0; i < mat.size(); i++)
-        cin >> mat[i][0];
-}
-
-template<typename T>
-void nMatrix(vector<vector<T>> &mat)
+void nMatrix(vector<vector<int>> &mat)
 {
     for (int i = 0; i < mat.size(); i++)
     {
@@ -56,33 +37,93 @@ void nMatrix(vector<vector<T>> &mat)
     }
 }
 
-template<typename T>
-    vector<vector<T>> transpositionVec(vector<vector<T>> mat)
+void pMatrix(vector<vector<int>> mat)
 {
-        vector<std::vector<T>> result(mat.size(), vector<T>(mat.size()));
+    for (int i = 0; i < mat.size(); i++)
+    {
+        for (int j = 0; j < mat[i].size(); j++)
+            cout << mat[i][j] << "\t";
+        cout << endl;
+    }
+}
 
+void aMatrix(vector<vector<int>> mat, vector<vector<int>> mat2)
+{
+    vector<vector<int>> result(mat.size(), vector<int>(mat.size()));
+    
+    if (mat.size() == mat2.size())
         for (int i = 0; i < mat.size(); i++)
         {
             for (int j = 0; j < mat.size(); j++)
-                result[i][j] = mat[j][i];
+            {
+                result[i][j] = mat[i][j] + mat2[i][j];
+                cout << result[i][j] << "\t";
+            }
+            cout << endl;
         }
-        return result;
+}
+
+void sMatrix(vector<vector<int>> mat, vector<vector<int>> mat2)
+{
+    vector<vector<int>> result(mat.size(), vector<int>(mat.size()));
+
+    if (mat.size() == mat2.size())
+        for (int i = 0; i < mat.size(); i++)
+        {
+            for (int j = 0; j < mat.size(); j++)
+            {
+                result[i][j] = mat[i][j] - mat2[i][j];
+                cout << result[i][j] << "\t";
+            }
+            cout << endl;
+        }
+}
+
+void mMatrix(vector<vector<int>> mat, vector<vector<int>> mat2)
+{
+    vector<vector<int>> result(mat.size(), vector<int>(mat.size()));
+    
+    if (mat.size() == mat2.size())
+        for (int i = 0; i < mat.size(); i++)
+        {
+            for (int j = 0; j < mat.size(); j++)
+            {
+                for (int k = 0; k < mat.size(); k++)
+                    result[i][j] += mat[i][k] * mat2[k][j];
+                cout << result[i][j] << "\t";
+            }
+            cout << endl;
+        }
+}
+
+void tMatrix(vector<vector<int>> &mat)
+{
+    vector<vector<int>> result(mat.size(), vector<int>(mat.size()));
+    
+    for (int i= 0; i < mat.size(); i++)
+    {
+        for (int j = 0; j < mat.size(); j++)
+        {
+            result[i][j] = mat[j][i];
+            cout << result[i][j] << "\t";
+        }
+        cout << endl;
     }
+}
 
 int main()
 {
-    cout << "Hello! I'm Triangular Matrix " << SIZE << "x" << SIZE << " Calculator!" << endl;
+    cout << "Hello! I'm Triangular Matrix ( " << SIZE << "x" << SIZE << " ) Calculator!" << endl;
     Menu();
     int exit = -1;
 
     vector<vector<int>> mat_1(SIZE, vector<int>(SIZE));
     vector<vector<int>> mat_2(SIZE, vector<int>(SIZE));
-    vector<vector<int>> vec(SIZE, vector<int>(SIZE));
     vector<vector<int>> result(SIZE, vector<int>(SIZE));
 
     while (exit != 0)
     {
-        cout << "Enter: ";
+        cout << "Enter number: ";
         cin >> exit;
     
         switch (exit)
@@ -90,64 +131,50 @@ int main()
             case 0: {break; };
             case 1:
             {
-                cout << "[i]: " << endl;
-                iMatrixTwo(vec);
-                printMatrixTwo(vec);
+                cout << "You Choose Enter Matrix A!" << endl;
+                nMatrix(mat_1);
+                iMatrix(mat_1);
+                cout << "Result: " << endl;
+                pMatrix(mat_1);
                 break;
             }
             case 2:
             {
-                cout << "[i][j]: " << endl;
-                nMatrix(mat_1);
-                iMatrixOne(mat_1);
-                printMatrixOne(mat_1);
+                cout << "You Choose Enter Matrix B!" << endl;
+                nMatrix(mat_2);
+                iMatrix(mat_2);
+                cout << "Result: " << endl;
+                pMatrix(mat_2);
                 break;
             }
             case 3:
             {
-                cout << "[i][j]: " << endl;
-                nMatrix(mat_2);
-                iMatrixOne(mat_2);
-                printMatrixOne(mat_2);
+                cout << "You Choose Matrix A + Matrix B!" << endl << "Result: " << endl;
+                aMatrix(mat_1, mat_2);
                 break;
             }
             case 4:
             {
-                int M;
-                
-                cout << "You chose transpose!" << endl << "Now choose matrix:" << endl << "1 - A, 2 - B;" << endl << "Enter: ";
-                cin >> M;
-                
-                switch (M)
-                {
-                    case 1:
-                    {
-                        result = transpositionVec(mat_1);
-                        printMatrixOne(result);
-                        break;
-                    }
-                    case 2:
-                    {
-                        result = transpositionVec(mat_2);
-                        printMatrixOne(result);
-                        break;
-                    }
-                }
+                cout << "You Choose Matrix A - Matrix B!" << endl << "Result: " << endl;
+                sMatrix(mat_1, mat_2);
+                break;
+            }
+            case 5:
+            {
+                cout << "You Choose Matrix A * Matrix B!" << endl << "Result: " << endl;
+                mMatrix(mat_1, mat_2);
+                break;
+            }
+            case 6:
+            {
+                cout << "You Choose Transpose Matrix A and Matrix B!" << endl << "Result: " << endl;
+                tMatrix(mat_1);
+                cout << endl;
+                tMatrix(mat_2);
                 break;
             }
         }
     }
+    
     return 0;
 }
-
-/*
- 
- int randomNumberGen(int max, int min)
- {
-     int num  = min + rand() % (max - min + 1);
-     return num;
- }
- 
-*/
- 
-
